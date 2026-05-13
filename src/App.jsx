ba063aff0648ca1d39bf4bfed52920b9355f9e1b
Dimas Wahyu <dimaswhyu@gmail.com>
2026-05-13 19:02:08 +0700

    Add global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
 import Portfolio from './pages/Portfolio'
+import './index.css';
 
 function App () {
   return (
